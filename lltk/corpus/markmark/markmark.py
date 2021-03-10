7dab96c8fa60e30b8e23829f8a26bdc20a475e1d
quadrismegistus <ryan.heuser@gmail.com>
2021-03-10 16:58:02 +0000

    fix

--- lltk/corpus/markmark/markmark.py
+++ lltk/corpus/markmark/markmark.py
@@ -4,4 +4,7 @@ class MarkMark(Corpus):
 	def load_metadata(self,*x,**y):
 		meta=super().load_metadata()
 		meta['genre']='Fiction'
+		for bc in {'author_id'}:
+			if bc in meta.columns:
+				meta=meta.drop(bc,1)
 		return meta
