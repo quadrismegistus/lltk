d90e06e29ad0fc2366f10b7d61e2f074ee86f59f
quadrismegistus <ryan.heuser@gmail.com>
2021-03-31 20:40:35 +0100

    vnum

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("requirements.txt", "r", encoding='utf-8', errors='ignore') as fh:
 
 setup(
     name='lltk-dh',
-    version='0.5.5',
+    version='0.5.6',
     description=('Literary Language Toolkit (LLTK): corpora, models, and tools for the digital humanities'),
     long_description=long_description,
     long_description_content_type="text/markdown",
